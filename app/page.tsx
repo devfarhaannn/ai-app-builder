7adf0ccf62a5122d5736848e0f4f66e0ed5cd077
devfarhaannn <bhattfarhan1804@gmail.com>
2026-08-04 18:55:19 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="pt-40">
+      <Button variant={`destructive`}>Welcome to forge Ai</Button>
+      </div>
+  );
+}
